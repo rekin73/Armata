5761b5da5297b1448b5a4691269b957719f9c188
rekin73 <runefor99@gmail.com>
2018-04-18 11:47:49 +0200

    Dokonczenie lufy

--- static/js/Armata.js
+++ static/js/Armata.js
@@ -1,20 +1,26 @@
 function Armata() {
     this.cannonAngle = function (angle) {
-        console.log("ruch lufy"+angle)
-        var vector = new THREE.Vector3(0, 1, 0);
-        armata.lufa.setRotationFromAxisAngle(vector, angle * Math.PI / 180);
+        console.log("ruch lufy" + angle)
+        var vector = new THREE.Vector3(1, 0, 0);
+
+        var lufa = armata.getObjectByName('lufa');
+        lufa.geometry.translate(0,0,1)
+        lufa.setRotationFromAxisAngle(vector, angle * Math.PI / 180);
     };
     this.cannonRotate = function (angle) {
         console.log("Rotate:" + angle)
         console.log(angle * Math.PI / 180)
         var vector = new THREE.Vector3(0, 1, 0);
         armata.setRotationFromAxisAngle(vector, angle * Math.PI / 180);
+        //armata.rotation.y=angle * Math.PI / 180
     };
 
     var geometry = new THREE.CylinderGeometry(2, 2, 10, 32);
-    var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    var material1 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+    var material = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xff0000 });
+    var material1 = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x0000ff });
     var lufa = new THREE.Mesh(geometry, material);
+    lufa.name = "lufa";
+
 
     var armata = new THREE.Object3D();
     var koloGeo = new THREE.CylinderGeometry(2, 2, 2, 32);
@@ -28,10 +34,12 @@ function Armata() {
     kolo2.position.x -= 3
 
 
-
+    var axes = new THREE.AxesHelper(1000)
     armata.add(lufa)
     armata.add(kolo)
     armata.add(kolo2)
+    armata.add(axes)
+    console.log(armata.getObjectByName('lufa'))
     this.getArmata = function () {
         return armata;
     }
--- static/js/Siatka.js
+++ static/js/Siatka.js
@@ -6,20 +6,22 @@ function Siatka() {
         0.1, // minimalna renderowana odległość
         10000 // maxymalna renderowana odległość
     );
-    camera.position.set(100,5,-10);
+    camera.position.set(-5,100,-50);
     var renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0xffffff);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     var axes = new THREE.AxesHelper(1000)
-    var planeGeo = new THREE.PlaneGeometry(1000, 1000, 20, 20)
+    var planeGeo = new THREE.PlaneGeometry(1000, 1000, 50, 50)
     var planeMesh = new THREE.MeshBasicMaterial({
         
         side: THREE.DoubleSide,
         wireframe:true,
+        color:"red"
     })
     var plane = new THREE.Mesh(planeGeo, planeMesh);
+    plane.rotateX(Math.PI/2)
 
 
 
@@ -30,6 +32,7 @@ function Siatka() {
 
     scene.add(axes);
     scene.add(armata.getArmata())
+    scene.add(plane)
     function render() {
 
         requestAnimationFrame(render);
@@ -39,12 +42,17 @@ function Siatka() {
         renderer.render(scene, camera);
     }
     $("#root").append(renderer.domElement);
-    camera.lookAt(scene.position)
+    camera.lookAt(armata.getArmata().position)
     render();
 
     $('#cRotate').on('input',function(){
         var angle=$('#cRotate').val();
         
         armata.cannonRotate(angle)
+    })
+    $('#cAngle').on('input',function(){
+        var angle=$('#cAngle').val();
+        
+        armata.cannonAngle(angle)
     })
 }
