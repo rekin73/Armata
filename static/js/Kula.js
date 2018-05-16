0bdbbefb95133edb7f84c25618fb162161d2c167
rekin73 <runefor99@gmail.com>
2018-05-16 10:50:17 +0200

    kula dodana

--- static/js/Kula.js
+++ static/js/Kula.js
@@ -1,3 +1,6 @@
 function Kula(){
-    var geometry=new THREE.SphereGeometry(2, 32, 32)
+    var geometry=new THREE.SphereGeometry(2, 32, 32);
+    var material = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x000000 });
+    var sphere=new THREE.Mesh(geometry,material);
+    armata.getArmata().add(sphere);
 }
